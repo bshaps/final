efd28f9b5298b714d2a38e006d233d1beb558560
Blake Shapiro <shapirob@uchicago.edu>
2015-05-07 13:37:41 -0500

    Everything but views complete

---
+++ app/controllers/carrier_controller.rb
@@ -0,0 +1,43 @@
+class CarriersController < ApplicationController
+
+	def index
+		@carrier = Carrier.all
+	end
+	
+	def show
+		@carrier = Carrier.find(params[:id])
+		if @carrier == nil
+			redirect_to carriers_url, notice: "Carrier Not Found!"
+		end
+	end
+	
+	def create
+		carrier = Carrier.new
+		carrier.name = params[:name]
+		carrier.mc_number = params[:mcnum]
+		carrier.dot_number = params[:dotnum]
+		carrier.save	
+
+		redirect_to carriers_url, notice: "Carrier created"
+	end
+	
+	def edit
+		@carrier = Carrier.find(params[:id])
+	end
+	
+	def update
+		carrier = Carrier.find(params[:id])
+		carrier.name = params[:name]
+		carrier.mc_number = params[:mcnum]
+		carrier.dot_number = params[:dotnum]
+		carrier.save
+		
+		redirect_to carriers_url
+	end
+	
+	def destroy
+		Carrier.delete(params[:id])
+		redirect_to carriers_url
+	end
+	
+end
