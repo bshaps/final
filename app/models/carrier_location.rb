a38ec873a0048ec7eb4b9f9a5e275e23bc02ecfe
Blake Shapiro <shapirob@uchicago.edu>
2015-05-21 14:26:48 -0500

    Validation updates

--- app/models/carrier_location.rb
+++ app/models/carrier_location.rb
@@ -1,5 +1,10 @@
 class CarrierLocation < ActiveRecord::Base
 	
 	belongs_to :carriers
+	has_one :cities
+	has_one :states
+	
+	validates :carrier_id, :city, :state, :zip, presence: true, numericality: {only_integer: true}
+	validates :street_addr, presence: true	
 	
 end
--- app/models/driver.rb
+++ app/models/driver.rb
@@ -2,5 +2,6 @@ class Driver < ActiveRecord::Base
 
 	belongs_to :carriers
 	validates_presence_of :name
+	validates :last_lat, :last_long, numericality: true
 	
 end
